4a94c324649f80dfbbc9879cfd1037fb241cae60
aditaurog-stack <adi.taurog@gmail.com>
2025-11-20 17:51:17 +0200

    Change prompt to ask for favorite movies

--- main.java
+++ main.java
@@ -1,12 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("What is your favorite color? ");
-        String color = input.nextLine();
+        System.out.print("Tell Me your favourite movies : ");
+        String line = input.nextLine();
 
-        System.out.println("You chose: " + color);
+        List<String> items = Arrays.asList(line.split(","));
+
+        System.out.println("Your movies: " + items);
     }
 }
